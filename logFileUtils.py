6f26d6c81f9fe210cb3117d711ed3870c48cd901
Maddy Adams <m20adams@seas.upenn.edu>
2022-12-01 05:03:16 +0000

    Added printing of last training loss per epoch, vertical lines at each epoch in graph

--- logFileUtils.py
+++ logFileUtils.py
@@ -15,7 +15,7 @@ def printLogFile(logFile, everyN=100, alwaysPrintLast=True):
         with open(logFile) as f:
             print(f.readlines()[-1])
 
-def graphLoss(logFile, everyN=100):
+def graphLoss(logFile, everyN=1):
     digitCtr = 0
 
     batches = []
@@ -31,6 +31,18 @@ def graphLoss(logFile, everyN=100):
                 else:
                     break
 
+    batchesPerEpoch = maxBatchN + 1
+
+    maxEpochN = 0
+    with open(logFile) as f:
+        for l in f.readlines():
+            if l[0].isdigit():
+                maxEpochN = max(maxEpochN, int(l.split("|")[0].strip()))
+
+    nEpochs = maxEpochN + 1
+
+    endOfEpochs = []
+
     with open(logFile) as f:
         for l in f.readlines():
             if l[0].isdigit():
@@ -40,10 +52,29 @@ def graphLoss(logFile, everyN=100):
                 epoch = int(l.split("|")[0].strip())
                 batch = int(l.split("|")[1].strip()) / 3600
                 loss = float(l.split("|")[2].strip())
-                batches.append((epoch * maxBatchN / 3600) + batch)
+                batches.append((epoch * batchesPerEpoch / 3600) + batch)
                 losses.append(loss)
 
+                if epoch >= len(endOfEpochs):
+                    endOfEpochs.append((epoch, batch, loss))
+                else:
+                    if endOfEpochs[-1][1] < batch:
+                        endOfEpochs[-1] = ((epoch, batch, loss))
+
+        
     plt.plot(batches, losses)
+
+    plt.plot([(e * batchesPerEpoch / 3600) + b for e, b, _ in endOfEpochs],
+             [l for _, _, l in endOfEpochs],
+             color="red")
+
+    print("training losses at end of epochs:")
+    for e, b, l in endOfEpochs:
+        print(f"{e} ({b}): {l}")
+
+    for i in range(nEpochs):
+        plt.axvline(i * batchesPerEpoch / 3600, color="black")
+
     plt.show()
 
 printLogFile("nohup.out")
